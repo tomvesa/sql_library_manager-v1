ae84e0dfc12397e30382645c5255ad3d8125bed3
tomvesa <47905734+tomvesa@users.noreply.github.com>
2023-10-05 21:31:58 +0300

    cleaning empty spaces

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,8 @@ var router = express.Router();
 /* GET home page. */
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  
-    res.redirect("books")
-  
-});
+      res.redirect("books")
+  });
 
 
 module.exports = router;
